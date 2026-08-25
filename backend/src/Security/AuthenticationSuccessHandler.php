d90e0643146ce5901b35ef26f86bed110b772acc
Jade <121947004+JadeB-95@users.noreply.github.com>
2026-08-25 12:56:21 +0200

    Mise à jour

--- backend/src/Security/AuthenticationSuccessHandler.php
+++ backend/src/Security/AuthenticationSuccessHandler.php
@@ -7,26 +7,26 @@ use Symfony\Component\HttpFoundation\Cookie;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 
-class AuthenticationSuccessHandler extends BaseHandler
-{
-    public function onAuthenticationSuccess($request, TokenInterface $token): JsonResponse
-    {
-        $response = parent::onAuthenticationSuccess($request, $token);
+// class AuthenticationSuccessHandler extends BaseHandler
+// {
+//     public function onAuthenticationSuccess($request, TokenInterface $token): JsonResponse
+//     {
+//         $response = parent::onAuthenticationSuccess($request, $token);
 
-        $data = json_decode($response->getContent(), true);
-        $jwt = $data['token'];
+//         $data = json_decode($response->getContent(), true);
+//         $jwt = $data['token'];
 
-        // cookie HTTP-only
-        $cookie = Cookie::create('JWT_TOKEN')
-            ->withValue($jwt)
-            ->withHttpOnly(true)
-            ->withSecure(false) // true en prod HTTPS
-            ->withSameSite('lax')
-            ->withPath('/');
+//         // cookie HTTP-only
+//         $cookie = Cookie::create('JWT_TOKEN')
+//             ->withValue($jwt)
+//             ->withHttpOnly(true)
+//             ->withSecure(false) // true en prod HTTPS
+//             ->withSameSite('lax')
+//             ->withPath('/');
 
-        $response->headers->setCookie($cookie);
+//         $response->headers->setCookie($cookie);
 
 
-        return $response;
-    }
-}
+//         return $response;
+//     }
+// }
